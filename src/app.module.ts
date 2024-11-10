cefb221e1939b90f7b3ef7cca65c3bce64fe57d7
katyastan <134015338+katyastan@users.noreply.github.com>
2024-11-10 23:50:58 +0300

    feat: add port value into .env, start openapi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ArtistsModule } from './artists/artists.module';
 import { AlbumsModule } from './albums/albums.module';
 import { TracksModule } from './tracks/tracks.module';
 import { FavoritesModule } from './favorites/favorites.module';
 
-
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UsersModule,
     ArtistsModule,
     AlbumsModule,
     TracksModule,
-    FavoritesModule
+    FavoritesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 4000);
-  console.log(`Application is running on port: ${process.env.PORT || 4000}`);
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service API')
+    .setDescription('API documentation for Home Library Service')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 4000;
+
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
