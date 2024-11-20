84f2573241df9153e58022ef9b83a7582299eb70
katyastan <134015338+katyastan@users.noreply.github.com>
2024-11-20 18:52:57 +0300

    feat: add exeption filter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import { LoggingService } from './logging/logging.service';
+import { AllExceptionsFilter } from './filters/exeptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const loggingService = app.get(LoggingService);
 
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter(loggingService));
 
   const config = new DocumentBuilder()
     .setTitle('Home Library Service API')
