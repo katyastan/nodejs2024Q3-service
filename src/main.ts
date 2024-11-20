03c01ea0dc3e073f38d55ef93c9e1f5da8114359
katyastan <134015338+katyastan@users.noreply.github.com>
2024-11-21 00:12:22 +0300

    feat: authentication is required except auth,doc,/

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,30 @@ import { LoggingService } from './logging/logging.service';
 import { AllExceptionsFilter } from './filters/exeptions.filter';
 import { Request, Response } from 'express';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const loggingService = app.get(LoggingService);
 
-  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter(loggingService));
-  app.useGlobalGuards(new JwtAuthGuard());
+  app.useGlobalGuards(
+    new (class extends JwtAuthGuard {
+      canActivate(context) {
+        const request = context.switchToHttp().getRequest();
+        const { url } = request;
+        if (
+          url === '/' ||
+          url.startsWith('/auth/') ||
+          url.startsWith('/doc')
+        ) {
+          return true;
+        }
+        return super.canActivate(context);
+      }
+    })(),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Home Library Service API')
